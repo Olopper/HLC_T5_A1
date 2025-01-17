312fa4a50e0e2c7db3caa90b4877bce7a2ee3caa
Olopper <olopper2407@g.educaand.es>
2025-01-17 12:20:26 +0100

    Exercise 6

---
+++ ejercicio6.py
@@ -0,0 +1,8 @@
+numero=int(input("Introduzca un número"))
+numero2=int(input("Introduzca un número"))
+if numero > numero2:
+    print(numero, "es mayor que", numero2)
+elif numero2 > numero2:
+    print(numero2, "es mayor que", numero)
+else:
+    print("Los números son iguales")
